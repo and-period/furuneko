358ab089ed6cd00c45e243493707fc822bd5de05
濵田広大 <hamany0326@gmail.com>
2023-09-16 18:35:41 +0900

    Add: added signin page ver1

---
+++ web/src/pages/signin.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+const onSubmit = (): void => {}
+</script>
+
+<template>
+  <v-card>
+    <v-card-title>ログイン</v-card-title>
+
+    <v-form @submit.prevent="onSubmit">
+      <img class="logo" alt="ふるマル ロゴ" src="/app-logo-with-title.svg" :width="450" >
+      <v-card-text>
+        <v-text-field required label="ユーザーID（メールアドレス)" type="email" />
+        <v-text-field required label="パスワード" />
+      </v-card-text>
+      <v-card-actions>
+        <v-btn type="submit" block color="primary" variant="outlined"> ログイン </v-btn>
+      </v-card-actions>
+    </v-form>
+  </v-card>
+</template>
+
+<style lang="scss" scoped>
+.logo {
+  height: auto;
+  display: block;
+  max-width: 80%;
+}
+</style>
