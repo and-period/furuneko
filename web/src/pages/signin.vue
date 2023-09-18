bd1f0bc90919d3a8ccdd11b0942e6dfe58d67b08
濵田広大 <hamany0326@gmail.com>
2023-09-19 00:05:00 +0900

    Fix: fixed title

--- web/src/pages/signin.vue
+++ web/src/pages/signin.vue
@@ -4,25 +4,20 @@ const onSubmit = (): void => {}
 
 <template>
   <v-card>
-    <v-card-title>ログイン</v-card-title>
-
+    <v-card-title class="mx-auto pt-8">
+      ふるねこログイン
+    </v-card-title>
     <v-form @submit.prevent="onSubmit">
-      <img class="logo" alt="ふるマル ロゴ" src="/app-logo-with-title.svg" :width="450" >
       <v-card-text>
         <v-text-field required label="ユーザーID（メールアドレス)" type="email" />
         <v-text-field required label="パスワード" />
       </v-card-text>
       <v-card-actions>
-        <v-btn type="submit" block color="primary" variant="outlined"> ログイン </v-btn>
+        <v-btn type="submit" rounded block color="primary" variant="outlined"> ログイン </v-btn>
       </v-card-actions>
     </v-form>
   </v-card>
 </template>
 
 <style lang="scss" scoped>
-.logo {
-  height: auto;
-  display: block;
-  max-width: 80%;
-}
 </style>
