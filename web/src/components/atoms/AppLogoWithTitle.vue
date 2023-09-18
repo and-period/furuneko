f7c168c7241570e39cb5366c8e2f2c3743f0f8e4
濵田広大 <hamany0326@gmail.com>
2023-09-19 00:58:51 +0900

    Add: added signin page

---
+++ web/src/components/atoms/AppLogoWithTitle.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+const props = defineProps({
+  width: {
+    type: Number,
+    default: 200
+  }
+})
+</script>
+
+<template>
+  <img
+    class="logo"
+    alt="ふるマル ロゴ"
+    src="/newlogo.svg"
+    :width="props.width"
+  >
+</template>
+
+<style lang="scss" scoped>
+.logo {
+  height: auto;
+  display: block;
+  max-width: 80%;
+}
+</style>
--- web/src/pages/signin.vue
+++ web/src/pages/signin.vue
@@ -3,21 +3,19 @@ const onSubmit = (): void => {}
 </script>
 
 <template>
-  <v-card>
-    <v-card-title class="mx-auto pt-8">
-      ふるねこログイン
-    </v-card-title>
-    <v-form @submit.prevent="onSubmit">
-      <v-card-text>
-        <v-text-field required label="ユーザーID（メールアドレス)" type="email" />
-        <v-text-field required label="パスワード" />
-      </v-card-text>
-      <v-card-actions>
-        <v-btn type="submit" rounded block color="primary" variant="outlined"> ログイン </v-btn>
-      </v-card-actions>
-    </v-form>
-  </v-card>
-</template>
+  <v-container>
+    <atoms-app-logo-with-title :width="300" class="mx-auto py-8" />
 
-<style lang="scss" scoped>
-</style>
+    <v-card width="80%" class="mx-auto mt-5">
+      <v-form @submit.prevent="onSubmit">
+        <v-card-text>
+          <v-text-field required label="ユーザーID（メールアドレス)" type="email" />
+          <v-text-field required label="パスワード" />
+        </v-card-text>
+        <v-card-actions>
+          <v-btn type="submit" rounded block color="primary" variant="outlined"> ログイン </v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card>
+  </v-container>
+</template>
